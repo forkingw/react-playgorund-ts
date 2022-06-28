5bdff20a7019faa40666296361973812ec09aa18
wmh <forkingw@outlook.com>
2022-06-28 13:53:09 +0800

    Update ts-palyground.tsx
    chore: 修改注释

--- src/routes/ts-palyground.tsx
+++ src/routes/ts-palyground.tsx
@@ -38,19 +38,19 @@
 // console.log(fn(function A (str: string) {return new Date()}))
 
 // Typescript 构造函数！！！！ 
-interface T {
-  s: string
-}
-type SomeConstructor = {
-  new (s: string): T;
-};
-function fn(ctor: SomeConstructor) {
-  return new ctor("hello");
-}
-class A {
-  constructor(public s:string) {}
-}
-fn(A)
+// interface T {
+//   s: string
+// }
+// type SomeConstructor = {
+//   new (s: string): T;
+// };
+// function fn(ctor: SomeConstructor) {
+//   return new ctor("hello");
+// }
+// class A {
+//   constructor(public s:string) {}
+// }
+// fn(A)
 
 // function firstElement<Type>(arr: Type[]): Type | undefined {
 //   return arr[0]
