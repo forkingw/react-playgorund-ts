8d0e8af3dffd90707c85a16625c6ede0343e3b8c
wmh <forkingw@outlook.com>
2022-06-30 20:03:03 +0800

    chore: arrangement code

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="react-scripts" />
 // declare module 'use-throttled-effect';
-declare module "use-throttled-effect" {
-  export default function useThrottledEffect(
-    callback: React.EffectCallback,
-    delay: number,
-    deps?: React.DependencyList | undefined
-  ): void;
-}
