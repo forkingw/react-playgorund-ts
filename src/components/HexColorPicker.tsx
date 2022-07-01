43c4910b261b2ebe850e822779f359660f9e11ff
wmh <forkingw@outlook.com>
2022-07-01 14:54:49 +0800

    zip: delete package-lock.json; add some changes for HexColorPicker.tsx

--- src/components/HexColorPicker.tsx
+++ src/components/HexColorPicker.tsx
@@ -2,26 +2,38 @@
 // import { ColorInput } from "./common/ColorInput"
 
 // import { ColorPicker } from "./common/ColorPicker"
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './styles.css'
 
+// const isTouch = (event: MouseEvent | TouchEvent): event is TouchEvent => "touches" in event
+
 export const HexColorPicker = () => {
-  const [top, setTop] = useState(0)
-  const [left, setLeft] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [top] = useState(0)
+  const [left] = useState(0)
   const handleMoveStart = (e: any) => {
     console.log('-=-=', e)
   }
-  const handleMove = (event: MouseEvent | TouchEvent) => {
-    console.log('evnet', event)
+  const handleMove = (event: any) => {
+    console.log('event', event)
   }
 
-  const toggleDocumentEvents = () => {
-
+  const handleMoveEnd = () => toggleDocumentEvents(false);
+  
+  const toggleDocumentEvents = (state?:boolean) => {
+    const el = containerRef.current
+    if (el) {
+      const toggleEvent = state ? el.addEventListener : el.removeEventListener
+      toggleEvent('mousemove', handleMove)
+      toggleEvent('mouseup', handleMoveEnd)
+    }
   }
+
+
 
   return (<div>
-    <div className="picker" onTouchStart={handleMoveStart} onMouseDown={handleMoveStart}>
-      <div className='pointer' style={{top, left}}></div>
+    <div ref={containerRef} className="picker" onTouchStart={handleMoveStart} onMouseDown={handleMoveStart} onMouseMove={handleMove}>
+      <div className='pointer' style={{ top, left }}></div>
     </div>
     <div className='hue'></div>
   </div>)
@@ -31,6 +43,7 @@ export const HexColorPicker = () => {
  * TODO can learn
  * 1. 如果判断有没有 touch 事件
  * 2. Typescript 的 is 关键词的使用
+ *      类型约束、保护函数 中有用到！
  * 3. mousemove 事件的使用
  * 4. 元素聚焦的使用 dom.focus
  */
